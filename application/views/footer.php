b62737b9e62522f71cbf0216154272e0c1f8fd29
huzaifa <huzaifamalam2310@gmail.com>
2023-03-19 15:13:31 +0530

    new file added

--- application/views/footer.php
+++
@@ -1,9 +0,0 @@
-<!-- css -->
-<link rel="stylesheet" href="<?php echo base_url("resource/css/bootstrap.min.css"); ?>">
-
-
-
-
-<!-- script -->
-<script src="<?php echo base_url("resource/js/jquery.js"); ?>"></script>
-<script src="<?php echo base_url("resource/js/bootstrap.min.js"); ?>"></script>
